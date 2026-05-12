dbaffd6aa89388d907de4d90ab405fd2db154353
Positronico <marcosmachado@gmail.com>
2026-05-12 12:15:26 -0700

    Update snapem to v0.4.0

--- Formula/snapem.rb
+++ Formula/snapem.rb
@@ -1,17 +1,17 @@
 class Snapem < Formula
   desc "Zero-Trust npm/bun CLI for macOS Silicon"
   homepage "https://github.com/Positronico/snapem"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Positronico/snapem/releases/download/v#{version}/snapem_#{version}_darwin_arm64.tar.gz"
-      sha256 "5bc00135bb3f5e37778608106143a2f98ac33f4f7a36c761365e279ccee70d2a"
+      sha256 "0e29995a24a5f0521a2cfaa0a7a1e663840718320c1b2477b84895ee627593dc"
     end
     on_intel do
       url "https://github.com/Positronico/snapem/releases/download/v#{version}/snapem_#{version}_darwin_amd64.tar.gz"
-      sha256 "c3de4c44977175ee6da51cc1d96165a4b83afedec6aaa4cdd250f0915a2c12a8"
+      sha256 "0bf1dbfa19ec949976bbd2b5ad246abae4713d64e95f397be6d2ade36439cb04"
     end
   end
 
